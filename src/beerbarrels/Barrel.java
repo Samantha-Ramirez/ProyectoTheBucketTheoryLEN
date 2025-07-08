a96e8a3715ca47c183e0c7d8b970ec5c0a0a8e66
MarcelM17 <124943566+MarcelM17@users.noreply.github.com>
2025-07-08 19:15:29 -0400

    Update Barrel.java

--- src/beerbarrels/Barrel.java
+++ src/beerbarrels/Barrel.java
@@ -39,13 +39,14 @@ public class Barrel {
             int spaceAvailable = maxCapacity - currentAmount;
             int overflow = amount - spaceAvailable;
             currentAmount = maxCapacity;
+            notifyAll();
+
         if (!id.equals("B")){
             System.out.println("El proveedor "+ supplier +" añadio " + spaceAvailable + "L al barril " + id + " (lleno). Litros actuales: " + currentAmount + ", desborde: " + overflow + "L");
          }
         else{
           System.out.println("El proveedor "+ supplier +" añadio por desborde " + spaceAvailable + "L al barril " + id + " (lleno). Litros actuales: " + currentAmount + ", desborde: " + overflow + "L");  
         }
-             notifyAll();
             
             // Transferir desborde a vecino, evitando barriles ya visitados
             if (id.equals("A")) {
