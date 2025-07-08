6bafb3d3f2bddaa10793424f3f7280b070a6fc93
MarcelM17 <124943566+MarcelM17@users.noreply.github.com>
2025-07-08 16:23:41 -0400

    Update BeerBarrels.java

--- src/beerbarrels/BeerBarrels.java
+++ src/beerbarrels/BeerBarrels.java
@@ -219,7 +219,6 @@ public class BeerBarrels {
             // Esperar a que todos los hilos de estudiantes terminen
             for (Thread thread : studentThreads) {
                 try {
-                    System.out.println("Esperar thread estudiante");
                     thread.join();
                 } catch (InterruptedException e) {
                     System.out.println("Error al esperar hilos de estudiantes: " + e.getMessage());
@@ -236,7 +235,6 @@ public class BeerBarrels {
             // Esperar a que todos los hilos de proveedores terminen
             for (Thread thread : supplierThreads) {
                 try {
-                    System.out.println("Esperar thread proveedor");
                     thread.join();
                 } catch (InterruptedException e) {
                     System.out.println("Error al esperar hilos de proveedores: " + e.getMessage());
